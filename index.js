97ba3b8cf1cd5664901fa3bdca0ce3769fc191ce
Valera97 <valerakot97@gmail.com>
2018-09-03 10:44:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -71,11 +71,8 @@ var site_url= {
         ]
     })
 };
-bot.onText(/\/msgid/, function (msg, match) {
-     bot.sendMessage(msg.chat.id, JSON.stringify(msg));
-});
 bot.onText(/\/start/, function (msg, match) {
-    bot.forwardMessage(chat.id, chat.id,"Text");
+    bot.sendMessage(msg.chat.259415250,"Text");
     bot.sendMessage(msg.chat.id, `Здраствуйте, ${msg.from.first_name}`);
     bot.sendMessage(msg.chat.id, "Выберите пожалуйста ниже - один из вариантов:", hi_keboard);
 });
